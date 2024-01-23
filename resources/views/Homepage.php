d4c3488ec29591baeab1ab9ab8dc4c8ce0a00426
aqilmaulana12345 <aqilmaulana99@gmail.com>
2024-01-23 14:45:23 +0700

    first commit

---
+++ resources/views/Homepage.php
@@ -0,0 +1,241 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" href="/css/style.css"integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=Outfit:wght@100;200;300;400;700&display=swap" rel="stylesheet"> 
+  </head>
+  <body>
+
+  <nav class="container-fluid navbar navbar-expand-lg " body style="background-color:rgba(21, 84, 141, 1);"   img src="graha.jpg" width="100%" class="img-fluid" alt="">
+    <a class="navbar-brand text-white" href="#"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+    <img src="graha.png" width="70px" class="img-fluid" alt="">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active text-white" aria-current="page" href="#">Homepage</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Tentang</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Daftar Hotel</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled text-white" aria-disabled="true">Bantuan</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"  body style="color:rgb(255, 255, 255);">
+            Language 
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Bahasa Indonesia</a></li>
+            <li><a class="dropdown-item" href="#">Bahasa Inggris</a></li>
+          </ul>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</nav>
+</div>
+
+<div class="container-fluid p-5 " body style="background-color:rgba(21, 84, 141, 1);">
+<div class="row">
+<div class="col-8 "></div>
+    <div  class="col-8 TulisanHome p-5"><h1 body style="color:rgb(255,255,255);  font-weight: bold;">Membantu Menjalankan</h1>
+    <h1 class="p-2" body style="color:rgb(255,255,255); font-weight: bold; font-family: 'Outfit', sans-serif;" >Operasi Bisnis Pariwisata Anda</h1>
+    <p class="p-0" body style="color:rgb(255,255,255); font-weight:thin; font-family: 'Outfit', sans-serif;">GWA membantu mengoperasikan keseluruhan layanan hotel, menajadikan mitra lebih percaya diri dalam menjalankan bisnis.</p> 
+    <div class="container">
+    <button body style="color:rgb(250,197,104 ,1);" type="button"  class="btn btn-warning "><p body style="color:rgb(0,0,0); border-radius:20px; width:214px; height:14px;" >Konsultasikan Bisnis Saya</p></button>
+    </div>
+</div>
+    <div class="col-4 container-fluid p-4">
+    <img src="rumah.jpg"   body style="width: 100rem; height:30rem;"; class="img-fluid" alt="">
+    </div>
+</div>
+</div>
+<div class="container-fluid p-5">
+  <h5 class="p-2"  body style= "font-family: 'Outfit', sans-serif; font-weight:400px;">- OUR  WORKFLOW</h5>
+<h1 class="tulisan p-2"  body style= "font-family: 'Outfit', sans-serif; font-weight:700px;">Proses Kerja Kami dalam Meningkatkan</h1><h1  body style= "font-family: 'Outfit', sans-serif; font-weight:700px;"> Kualitas Properti</h1>
+<div class="row p-5">
+    <div class="col p-4">
+    <img src="11.png" width="150px" class="img-fluid" alt="">
+    <h1 class="p-1" body style= "font-family:'Outfit', sans-serif; font-weight:300px;">Tantangan</h1>
+    <p  class="p-1" body style= "font-family:'Outfit', sans-serif; font-weight:100px;">Setiap property memiliki tantangan tersendiri dan GWA sangat memahami hal tersebut.</p>
+    </div>
+    <div class="col p-0">
+    <img src="12.png" width="150px" class="img-fluid" alt="">
+    <h1 body style="font-family: 'Outfit', sans-serif; font-weight:700px;">Rumusan</h1>
+    <p body style= "font-family: 'Outfit', sans-serif; font-weight:100px;">Setiap tantangan akan dirumuskan menjadi sebuah hal yang harus dicari jalan keluarnya oleh kami</p>
+    </div>
+    <div class="col p-4">
+    <img src="13.png" width="150px" class="img-fluid" alt=""> 
+    <h1 class="p-1" body style= "font-family: 'Outfit', sans-serif; font-weight:700px;">Goals</h1>
+    <p class="p-1" body style= "font-family: 'Outfit', sans-serif; font-weight:700px;">Setiap property memiliki tantangan tersendiri dan GWA sangat memahami hal tersebut.Dari rumusan masalah, kami memberikan respon cepat dan jalan keluar jangka pendek - menengah - panjang.</p>
+    </div>
+    <div class="col p-0">
+    <img src="14.png" width="150px" class="img-fluid" alt=""> 
+    <h1 body style= "font-family: 'Outfit', sans-serif; font-weight:700px;">Ideas</h1>
+    <p body style= "font-family: 'Outfit', sans-serif; font-weight:100px;">Setiap rumusan menjadikan ide-ide kreatif bagi kami untuk meningkatkan kualitas property mitra.</p>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="container-fluid p-5" body style="background-color:rgb(240,248,255);">
+  <p class="tilisan p-2"  body style="color:rgb(0, 0, 0); font-family: 'Outfit', sans-serif;">- OUR  SERVICES</p>
+<h1 class="tulisan p-2" body style="color:rgb(0, 0, 0); font-family: 'Outfit', sans-serif;">Apa Saja yang Bisa Kami Bantu ?</h1>
+<div class="row p-5">
+<div class="row">
+    <div class="col-6 col-md-4"><div class="card" style="width: 18rem;">
+  <img src="management.png"  body style="text-align:center ; width:120px;" ><h3 body style="color:rgb(250,197,104 ,1);">01</h3>
+  <div class="card-body">
+    <h5 class="card-title" body style= "font-family:Outfit;">Revenue Management Service</h5>
+  </div>  
+  <div class="container">
+  <button body style="color:rgb(250,197,104 ,1);" type="button"  class="btn btn-warning "><p body style="color:rgb(0,0,0); border-radius:80px; width:220px; height:28px; font-family:Outfit;" >Konsultasikan Bisnis Saya</p></button>
+</div>
+</div>
+</div>
+    <div class="col-6 col-md-4"><div class="card" style="width: 18rem;">
+    <img src="checklist.png"  style="width:150px" class="card-img-top" alt="..."> <h3 body style="color:rgb(250,197,104 ,1);">02</h3>
+  <div class="card-body">
+    <h5 class="card-title" body style= "font-family:Outfit;">Full Manage Service</h5>
+    <button body style="color:rgb(250,197,104 ,1);" type="button"  class="btn btn-warning "><p body style="color:rgb(0,0,0); border-radius:80px; width:220px; height:28px; font-family:Outfit;"  >Konsultasikan Bisnis Saya</p></button>
+  </div>
+</div></div>
+    <div class="col-6 col-md-4"><div class="card" style="width: 18rem; height:20rem;">
+    <img src="monitor.png"  style="width:150px;" class="card-img-top" alt="..."> <h3 body style="color:rgb(250,197,104 ,1);">03</h3>
+  <div class="card-body">
+    <h5 class="card-title" body style= "font-family:Outfit;">Asset Monetize Service</h5>
+    <button body style="color:rgb(250,197,104 ,1);" type="button"  class="btn btn-warning "><p body style="color:rgb(0,0,0); border-radius:80px; width:220px; height:28px; font-size:12px font-family:Outfit;"  >Konsultasikan Bisnis Saya</p></button>
+  </div>
+</div></div>
+  </div>
+</div>
+</div>
+<div class="container p-5" >
+  <h5 body style="text-align:center; color:rgb(0, 0, 0); font-family: 'Outfit', sans-serif;" font-weight:200; class="p-3">OUR LATEST PROJECT</h5>
+  <h1 body style="text-align:center; color:rgb(0, 0, 0); font-family: 'Outfit', sans-serif;" font-weight:800; class="p-1">Project Terbaru Kami</h1>
+  <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false">
+  <div class="carousel-inner" body style="width: 1182px; height:380px;">
+    <div class="carousel-item active"  >
+      <img src="Guesthouse.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="Guesthouse.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="Guesthouse.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev"  type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" body style="background-color:rgb(250,197,104)" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
+    <span class="carousel-control-next-icon"  body style="background-color:rgb(250,197,104)" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</div>
+<div class="container-fluid " body style="background-color:rgb(240,248,255);">
+<div class="row">
+    <div class="col-8 p-5 " body style;><h5 body style= "font-family: 'Outfit', sans-serif; font-weight:700px; " >Our Testimonials</h5>
+    <h1  body style= "font-family: 'Outfit', sans-serif; font-weight:600px; font-size:36px;" > Yang Klien Kami Katakan</h1>
+    <p class="p-5" body style= "font-family: 'Outfit', sans-serif; font-weight:300px; font-size:26px; line-height:normal;">  &#8220 GWA Group membantu hotel
+      saya untuk menjangkau lebih banyak client 
+      dan memberikan saran monetisasi yang tidak pernah saya bayangkan sebelumnya</p>
+      <div class="container" body style="text-align:center;">
+  <img src="yellow.png"   body style="width: 68px; height:68px; ; class="img-fluid alt="" ><h5 body style= "font-family: 'Outfit', sans-serif; font-weight:700px;">Chaim Desmon</h5> 
+  <p body style= "font-family: 'Outfit', sans-serif; font-weight:700px;">CEO of Yellow Hotel</p></div>
+</div>
+    <div class="col-4"> <img src="manager.jpg"   body style="width: 580px; height:500px; radius:60px, 0px, 0px, 60px  top:2951px"; class="img-fluid" alt=""></div>
+  </div>
+</div>
+<div class="container-fluid p-5">
+<div class="row">
+    <div class="col-sm-6 p-5">
+      <h5 body style= "font-family: 'Outfit', sans-serif; font-weight:300px;" >Hubungi kami </h5>
+      <h1 body style= "font-family: 'Outfit', sans-serif; font-weight:700px;" >Ingin Mendiskusikan
+Bisnis Pariwisata Anda ? </h1>
+      <p body style= "font-family: 'Outfit', sans-serif; font-weight:200; font-height:48px;">Ada kepentingan bisnismu yang ingin didikusikan dengan kami, 
+      yuk segera isi form disamping, konsultasinya gratis kok ! </p>
+    </div>
+    <div class="col-6 container-fluid" body style="background-color:rgba(248, 248, 248, 1);">
+    <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Nama Lengkap</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text"></div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Nama Perusahaan </label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Nomor WhatsApp Aktif </label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <button body style="color:rgb(250,197,104 ,1);" type="button"  class="btn btn-warning "><p body style="color:rgb(0,0,0); border-radius:80px; width:680px; height:18px; font-family:Outfit;" >Jadwalkan Konsultasi </p></button>
+</form>
+  </div>
+  </div>
+</div>
+<div class="container-fluid p-5"   body style="background-color:rgba(21, 84, 141, 1);">
+<img src="graha.png"   body style="width: 192px; height:100px; ">
+<div class="row p-2">
+    <div class="col"><h5 body style="color:rgba(255,255,255 );  font-family: 'Outfit', sans-serif; font-weight:300px;">Halaman Utama</h5></div>
+    <div class="col"> <h5 body style="color:rgba(255,255,255 ); font-family: 'Outfit', sans-serif; font-weight:300px;">Perusahaan</h5></div>
+    <div class="col"><h5 body style="color:rgba(255,255,255 );  font-family: 'Outfit', sans-serif; font-weight:300px;">Temukan</h5></div>
+    <div class="col"><h5 body style="color:rgba(255,255,255 );  font-family: 'Outfit', sans-serif; font-weight:300px;">Sosial Media</h5></div>
+  </div>
+  <div class="row ">
+    <div class="col">
+    <p body style="color:rgba(255,255,255 );  font-family: 'Outfit', sans-serif; font-weight:300px;">Homepage</p>
+  </div>
+    <div class="col">
+    <p body style="color:rgba(255,255,255 );  font-family: 'Outfit', sans-serif; font-weight:100px;">Tentang Kami</p>
+    <p body style="color:rgba(255,255,255 );  font-family: 'Outfit', sans-serif; font-weight:100px;">Daftar Hotel</p>
+    </div>
+    <div class="col">
+    <p body style="color:rgba(255,255,255 );  font-family: 'Outfit', sans-serif; font-weight:100px;">Bantuan</p>
+    <p body style="color:rgba(255,255,255 );  font-family: 'Outfit', sans-serif; font-weight:100px;">Hubungi Kami</p>
+    </div>
+    <div class="col ">
+      <div class="row"> 
+    <img src="instagram.png"  style="width:50px" class="card-img-top" alt="..."></div>
+    </div>
+  </div>
+</div>
+
+<div class="container-fluid p-5"   body style="background-color:rgba(21, 84, 141, 1);">
+  <div class="row align-items-start">
+    <div class="col">
+      <p body style="color:rgba(255,255,255 );  font-family: 'Outfit', sans-serif; font-weight:100px;">Pemberitahuan Privasi </p>
+      <p body style="color:rgba(255,255,255 );  font-family: 'Outfit', sans-serif; font-weight:100px;">Syarat dan Ketentuan </p>
+    </div>
+    <div class="col">
+    <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"  body style="color:rgb(255, 255, 255);">
+            Language 
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Bahasa Indonesia</a></li>
+            <li><a class="dropdown-item" href="#">Bahasa Inggris</a></li>
+          </ul>
+        </li>
+    </div>
+  </div>
+</div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
